9fe083a794e46a09e5b707dff093c75cc49c40c9
Paranoid Android <113983506+paranoid-android12@users.noreply.github.com>
2026-05-21 23:26:29 +0800

    remove bluir

--- src/components/GameInterface.tsx
+++ src/components/GameInterface.tsx
@@ -2125,7 +2125,6 @@ export const GameInterface: React.FC = () => {
                             left: 0,
                             right: 0,
                             bottom: 0,
-                            backgroundColor: "rgba(0, 0, 0, 0.3)",
                             display: "flex",
                             alignItems: "flex-end",
                             justifyContent: "center",
